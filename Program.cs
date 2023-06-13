d1be5e9e1d018d9b90a22114d3ba7dab8d34c7d3
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-06-13 17:27:40 +0200

    ajout de boucle sur des tableaux et imbrications de boucles pour creer une table de multiplication

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] myIntArray = new int[5] {1,2,3,4,5};
+
+Console.WriteLine("avec la boucle for");
+for (int i = 0; i < myIntArray.Length; i++)
+{
+    Console.WriteLine($"index {i} : {myIntArray[i]}");
+} 
+
+Console.WriteLine("avec la boucle foreach");
+foreach (int number in myIntArray){
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("avec la boucle while");
+int count = 0;
+
+while (count < myIntArray.Length)
+{
+    Console.WriteLine(myIntArray[count]);
+    count++;
+
+}
+
+Console.WriteLine("avec la boucle do while"); // utile uniquement si le code doit s'effectuer au minimum une fois
+int count2 = 0;
+
+do
+{
+  Console.WriteLine(myIntArray[count2]);
+    count2++;  
+} while (count2 < myIntArray.Length);
+
+// faire une table de multiplication avec boucles for imbriqués
+Console.WriteLine();
+Console.WriteLine("les tables de multiplications");
+for (int i = 1; i<=10; i++) {
+
+    // utilisation de ternaire pour l'affichage
+    Console.Write("   |  ");
+    Console.Write(i<10 ? i + "  |  " : i + " |  ");
+
+    for (int j = 2; j <= 10; j++)
+    {
+        
+        if (i*j == 100) {
+            Console.Write( i*j + "|");
+        
+        } else {
+
+            Console.Write(i*j<10 ?  i*j + "  |  " : i*j + " |  ");
+        }
+    }
+    Console.WriteLine();
+}
